b5b92f0719d418de9a49ec9b1731e6f818ba756b
gamburg <ulsk0073@gmail.com>
2025-10-25 17:46:32 +0400

    add TestCafeCount

--- main_test.go
+++ main_test.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCafeNegative(t *testing.T) {
@@ -28,6 +30,35 @@ func TestCafeNegative(t *testing.T) {
 
 		assert.Equal(t, v.status, response.Code)
 		assert.Equal(t, v.message, strings.TrimSpace(response.Body.String()))
+	}
+}
+
+func TestCafeCount(t *testing.T) {
+	handler := http.HandlerFunc(mainHandle)
+
+	requests := []struct {
+		count int
+		want  int
+	}{
+		{0, 0},
+		{1, 1},
+		{2, 2},
+		{100, len(cafeList["moscow"])},
+	}
+
+	for _, v := range requests {
+		reqStr := fmt.Sprintf("/cafe?city=moscow&count=%d", v.count)
+		req := httptest.NewRequest("GET", reqStr, nil)
+		response := httptest.NewRecorder()
+		handler.ServeHTTP(response, req)
+
+		respLen := 0
+		if response.Body.String() != "" {
+			respLen = len(strings.Split(response.Body.String(), ","))
+		}
+
+		require.Equal(t, http.StatusOK, response.Code)
+		assert.Equal(t, v.want, respLen)
 	}
 }
 
